a6e2c24e89e821a31d743b050af7f9c1fb84a5c0
veto <veto@lab.local>
2025-10-15 04:45:46 +0700

    update

--- ask.sh
+++ ask.sh
@@ -5,13 +5,14 @@ DATE=$(date +"%F")
 
 
 echo -e "I'm ask.sh. What you like to do?, enter a Task Id from list below: \n"
-echo -e "TaskID\tFile\t\tDescription"
-echo -e "1\t./Export Db\t\texport the database on the docker/test server"
-echo -e "2\t./Rename WP\t\trename the database on the docker server"
-echo -e "3\t./Clean Docker\tclean the docker containers and volumes "
-echo -e "4\t./Clean All Docker\tclean the docker containers and volumes and images "
-echo -e "5\t./WPCLI\t get into Wp cli "
-echo -e "6\t./Generate PHP docs\t - generate php docs into pages/public/docs "
+echo -e "TaskID\t Description"
+echo -e "1\t Export Db - Export the database on the docker/test server"
+echo -e "2\t Rename WP - Rename the database on the docker server"
+echo -e "3\t Clean Docker - Clean the docker containers and volumes "
+echo -e "4\t Clean All - Clean the docker containers and volumes and images "
+echo -e "5\t WPCLI - Get into Wp cli "
+echo -e "6\t Gen PHP docs - Generate php docs into pages/public/docs "
+echo -e "7\t Run - Docker Test Enviroment "
 
 
 read task
@@ -46,7 +47,12 @@ elif [ "$task" = "5" ]; then
     
 elif [ "$task" = "6" ]; then
     echo "... ${task} -- generate php docs"
-    phpDocumentor run -d  test/wordpress/wp-content/plugins/domain-translate/  -t pages/public/docs/    
+    phpDocumentor run -d  test/wordpress/wp-content/plugins/domain-translate/  -t pages/public/docs/
+
+elif [ "$task" = "7" ]; then
+    echo "... ${task} -- Run Docker Test"
+    cd test
+    docker-compose up -d
 else
     echo "Goodbye! - Exit"
 fi
